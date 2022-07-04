b85e44dcbc7bdb67e67445acc28e319ba6afb88d
Schattauer <markus.schattauer@fujitsu.com>
2022-07-04 10:54:51 +0200

    Fixed Bugs

--- receipt/tests.py
+++ receipt/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
+from .models import Receipt
 
 # Create your tests here.
 
 class URLTest(TestCase):
+
     def test_homepage(self):
         response=self.client.get("/")
         self.assertEqual(response.status_code,200)
+
+    def test_homepage_FAILURE(self):
+        response=self.client.get("/")
+        self.assertEqual(response.status_code,400)
 
--- receipt/views.py
+++ receipt/views.py
@@ -5,6 +5,12 @@ from random import *
 # Create your views here.
 
 def random(request):
+    # Adding two Recipes bc of Djangos Test DB Functionality - otherwise tests would fail
+    # just for development
+    recipe=Receipt.objects.create(name="Nudeln",picture=".",ingredients="Liste",instructions="Noch ne Liste")
+    recipetwo=Receipt.objects.create(name="Keine Nudeln",picture=".",ingredients="Liste",instructions="Noch ne Liste")
+
+    # here its normal again lol
     nrOfReceipts=len(Receipt.objects.all())
     receiptId=randint(1,nrOfReceipts)
     return receiptByID(request,receiptId)
