b31a9599e7cd2f88d11f6753f1db4075140ce57e
Schattauer <markus.schattauer@fujitsu.com>
2022-07-04 10:58:10 +0200

    Removed Failure-Test - Now it should work!

--- receipt/tests.py
+++ receipt/tests.py
@@ -9,7 +9,4 @@ class URLTest(TestCase):
         response=self.client.get("/")
         self.assertEqual(response.status_code,200)
 
-    def test_homepage_FAILURE(self):
-        response=self.client.get("/")
-        self.assertEqual(response.status_code,400)
 
